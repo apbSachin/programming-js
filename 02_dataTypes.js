49ac1f13f1af0e1aae7427bda5c1fa7f6512eab0
Sachin Biradar <139945811+apbSachin@users.noreply.github.com>
2023-08-18 07:20:16 +0000

    dataTypes

---
+++ 02_dataTypes.js
@@ -0,0 +1,14 @@
+"use strict"; //treat all js code to newer version 
+
+let name = "Sachin";
+let age = 23;
+let isLoggedIn = true;
+let city;
+
+//null: stand alone value retun type object
+//undefined: declared variale with no value;
+//number: range is 2 power of 53
+//boolean: return true or false 
+//bigInt: with biginit we can store large number value 
+//string: store the sequence of char in single or double qoute
+//symbol:symbols is to create unique property keys
