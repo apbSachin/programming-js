f0134b4cf257781a12ee5fbcb5a0f7d574ce02b3
Sachin Biradar <139945811+apbSachin@users.noreply.github.com>
2023-08-19 14:19:17 +0000

    comparison

---
+++ 03_comparison.js
@@ -0,0 +1,13 @@
+//******null*****/
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+//**undefined */
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+
+//string and number
+console.log("2" == 2);
