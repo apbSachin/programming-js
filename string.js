63d0acb761a77985fe53717ecfca3ca643596403
Sachin Biradar <139945811+apbSachin@users.noreply.github.com>
2023-09-12 11:16:53 +0000

    .

--- string.js
+++ string.js
@@ -27,4 +27,24 @@ console.log(newString.trim()); //it removes whitespace of start and end of the s
 console.log(newString.trimStart()); //it removes the trim of start of the string
 console.log(newString.trimEnd()); //it removes the trim of end of the string
 
+//**replace() */
+const url="https://sachinbiradar%20gmail.com"; //if you give space it ll be consider as %20
+const result=url.replace('%20','@');
+console.log(result);
+console.log(url.includes('sachin'));  //it ll give true or false based on whether the string contain that value or not
+
+
+//**split() : takes string and divides substring based on seperator and put that into array */
+const newName="Virat_Kohli_Dhoni";
+console.log(newName.split('_'));
+
+//**padStart(): it pads the one string with another string */
+const str1 = '5';
+console.log(str1.padStart(4, '0'));
+
+
+//**padEnd():it pads the at the end of the string */
+const str2='9';
+console.log(str2.padEnd(4,'0'));
+
 
