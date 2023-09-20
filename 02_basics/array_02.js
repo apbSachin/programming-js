f2d46959eddbccaa58ca6bd4598a3ab360ad42f4
Sachin Biradar <139945811+apbSachin@users.noreply.github.com>
2023-09-20 09:55:40 +0000

    .

---
+++ 02_basics/array_02.js
@@ -0,0 +1,25 @@
+const indianPlayers = ["Virat", "MSD", "KLRahul"];
+const sriLankanPlayers = ["Pathirana", "Malinga", "Snagakara"];
+//if i want to merge two arrays by push method the output ll be like below
+//[ 'Virat', 'MSD', 'KLRahul', [ 'Pathirana', 'Malinga', 'Snagakara' ] ] it like one array in another array
+//bad way to do like this
+
+//indianPlayers.push(sriLankanPlayers);
+
+//we can also use cancat() method to merge two arrays
+const bothTeam = indianPlayers.concat(sriLankanPlayers);
+console.log(bothTeam);  //[ 'Virat', 'MSD', 'KLRahul', 'Pathirana', 'Malinga', 'Snagakara' ]
+
+//**better way to do using spread operator */
+const IplBatsman = [...indianPlayers, ...sriLankanPlayers];
+console.log(IplBatsman); //[ 'Virat', 'MSD', 'KLRahul', 'Pathirana', 'Malinga', 'Snagakara' ]
+
+//if u have array inside another array and u want final array than use below syntax
+const arr1 = [1, 2, 3, [4, 5], 6, [7, 8, [9, 10]]]; //combine all array and subarray and give final array
+const result = arr1.flat(Infinity);//return a new array with all sub arrays concatinated
+console.log(result);
+
+//if u want to convert string into array then see below solutions
+const name = "SachinBiradar";
+console.log(Array.isArray(name)); //false :its is string 
+console.log(Array.from(name)); // [S', 'a', 'c', 'h','i', 'n', 'B', 'i',r', 'a', 'd', 'a','r'];
