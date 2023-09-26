cb6f0f426e8f0272b72eb35c00132c92b3a274a0
apbSachin <sba40599@gmail.com>
2023-09-26 12:41:30 +0530

    .

--- Node/app.js
+++ Node/app.js
@@ -1,1 +1,18 @@
-cosole.log('hello');
+//global module: which we are not importing or bydefault modules
+console.log("hello");
+
+//nonglobal : we are importing some outside modules
+const fs = require("fs");
+fs.writeFileSync("hello.js", "hello");
+
+//createSever(request,response): to use createServer we need to require http server
+//by taking response from createserverwe can use all responses methods
+//once we use method donot forgot to end the response 
+//than pass localhost to run your application
+const http = require("http");
+http
+  .createServer((req, resp) => {
+    resp.write("hello world");
+    resp.end();
+  })
+  .listen(8080);
